f7f0b1a85e7c1500199df94bb564d19c10899839
CLeano <cleano2@my.bcit.ca>
2022-11-22 10:23:21 -0800

    remove event listeners

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,8 +1,30 @@
 const clickMeButton = document.querySelector(".clickMe");
 
+// SAME
 function getAlert() {
     alert("POWERED UP!");
+    clickMeButton.removeEventListener ("click", getAlert) 
 }
 
-clickMeButton.addEventListener ("click", getAlert) 
+clickMeButton.addEventListener ("click", getAlert); 
+
+// SAME --- only shows once
+// function getAlert() {
+//     alert("POWERED UP!");
+//     // clickMeButton.removeEventListener ("click", getAlert) 
+// }
+
+// Does not show since removed
+// clickMeButton.addEventListener ("click", getAlert, {once:true}); 
+// clickMeButton.removeEventListener ("click", getAlert) 
 // DO NOT PUT getAlert() inside the eventListener ---- because this calls the function and the alert will show before clicking or before event
+
+// Should be same but not working !!!!!!!!!!!!!!!!!!!!!!!
+
+// clickMeButton.addEventListener ("click", function getAlert() {
+//     alert("POWERED UP!");
+// });  
+
+// clickMeButton.removeEventListener ("click", function getAlert() {
+//     alert("POWERED UP!");
+// }); 
